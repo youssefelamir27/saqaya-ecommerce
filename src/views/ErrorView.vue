a14da8f9b73f9fe8619a6535dff0a4283a413c5d
Youssef Elamir <youssefelamir27@gmail.com>
2026-04-02 16:26:15 +0200

    clean up commented code in 404 page

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,21 +1,3 @@
-<!-- <template>
-  <div class="error">
-    <h1>404 Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
-    <h3>Recommended Actions</h3>
-    <ul>
-      <li>Check the URL for typos or errors.</li>
-      <li>Return to the <a href="/">home page</a>.</li>
-      <li>Use the navigation menu to find the desired page.</li>
-    </ul>
-  </div>
-</template>
-<script>
-export default {
-  name: 'ErrorView',
-};
-</script> -->
-
 <template>
   <div class="error-page">
     <div class="breadcrumb">
