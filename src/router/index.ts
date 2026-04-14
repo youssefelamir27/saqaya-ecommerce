37e732e9ff19fa489ce894c110bed332ea5aecff
Youssef Elamir <youssefelamir27@gmail.com>
2026-04-14 18:44:39 +0200

    feat: add scrollBehavior to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -47,7 +47,7 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
   scrollBehavior() {
-    return { x: 0, y: 0 }; // ← always scroll to top on navigation
+    return { x: 0, y: 0 }; // 7always scroll to top on navigation
   },
 });
 
